174d07553d4dde9f7d4ede3bec2efc7c94e456f7
aakashkr <aspakash000@gmail.com>
2024-03-03 22:45:12 +0530

    changes made in texts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Header from "./components/Header";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache,
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <div className="container">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+export default function () {
+  return (
+    <>
+      <nav className="navbar bg-light mb-4 p-0 ">
+        <div className="container">
+          <a href="/" className="navabr-brand">
+            <div className="d-flex">
+              <img src="" alt="logo" className="mr-2" />
+              <div>Task Management</div>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+}
